db3a267b3a5d2b5ad8630686170d4fee926a268c
Nick <latentdev@gmail.com>
2015-10-10 07:13:52 -0700

    update

--- 2dArray/Array2D.cpp
+++ 2dArray/Array2D.cpp
@@ -1,17 +1,26 @@
 #include "Array2D.h"
+#include "Array.h"
 
 template<typename a_type>
 inline Array2D<a_type>::Array2D()
 {
 	m_array = 0;
+	m_row = 0;
+	m_col = 0;
 }
 
 template<typename a_type>
 inline Array2D<a_type>::Array2D(int in_row, int in_col)
 {
-	m_array = 0;
+	m_array = new a_type*[in_row*in_col];
+	//Array<Array<a_type>>b(in_row, 0);
+	//m_array =Array<a_type>(in_row,0);
+	//for (int i = 0; i < in_row; i++)
+		//b[i] = new Array<a_type>(in_col,0);
 	m_row = in_row;
 	m_col = in_col;
+	for (int i = 0; i < (in_row*in_col); i++)
+		m_array[i] = i;
 }
 
 template<typename a_type>
@@ -23,5 +32,8 @@ inline Array2D<a_type>::Array2D(const Array2D& in_copy)
 template<typename a_type>
 inline Array2D<a_type>::~Array2D()
 {
+//	for (int i = 0; i < m_row; ++i)
+	//	delete[] m_array[i];
 
+	//delete[] m_array;
 }
