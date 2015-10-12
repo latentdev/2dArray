bc70bb2af6bb21dd7e3da05307f607f70672f9f6
Nick <latentdev@gmail.com>
2015-10-11 19:10:58 -0700

    final update

--- 2dArray/Main.cpp
+++ 2dArray/Main.cpp
@@ -1,19 +1,22 @@
-#include "Array2D.h"
-#include "Array2D.cpp"
-#include <iostream>
-using namespace std;
-
+#include "driver.h"
+/*************************************************************************
+*Author:					Nicholas Olson
+*Date Created:				10-7-15
+*Last Modification Date:	10-11-15
+*Lab Number:				CST 211 Assignment 2
+*Filename:					Main.cpp
+*
+*Overview:
+*	Entry point of program
+*
+*Input:
+*	All the input is already set in the driver file. Only exists to show the uses of my array class.
+*
+*Output:
+*	driver() defined in Driver.cpp will walk through several operations involving the use of my array class and output it's progress and the results
+**************************************************************************/
 int main()
 {
-	Array2D<int> a;
-	Array2D<int> b(5, 2);
-	cout << b[1][1] << endl;
-	/*for (int i = 0; i < b.getRow(); i++)
-	{
-		for (int m = 0; m < b.getColumn(); i++)
-		{
-			cout << b[i][m] << endl;
-		}
-	}*/
+	driver();
 	return 0;
 }
